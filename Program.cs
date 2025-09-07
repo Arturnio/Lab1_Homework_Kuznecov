2c2e34ca85284f9c1a746e802df4adb5edd590e7
Artur K <only_up_ak@gmail.com>
2025-09-07 22:44:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab1_Homework_Kuznecov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("число 1: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("число 2: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{a + b}\n{a - b}\n{a * b}\n{(double)a / b}");
+
+            //2
+            //Console.Write("имя: ");
+            //string name = Console.ReadLine();
+            //Console.Write("возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("язык: ");
+            //string lang = Console.ReadLine();
+            //Console.WriteLine($"Привет {name}\nТебе {age} лет\nТвой любимый язык программирования - {lang}");
+
+            //3
+
+        }
+    }
+}
