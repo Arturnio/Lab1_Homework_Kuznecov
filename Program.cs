e9c7edcf82dcfae33a126a353ce05910d2d9f61b
Artur K <only_up_ak@gmail.com>
2025-09-07 23:07:37 +0300

    end

--- Program.cs
+++ Program.cs
@@ -21,7 +21,42 @@
             //Console.WriteLine($"Привет {name}\nТебе {age} лет\nТвой любимый язык программирования - {lang}");
 
             //3
-            плавопырвоп
+            //Console.Write("число 1: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("число 2: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("число 3: ");
+            //int c = int.Parse(Console.ReadLine());
+            //int x = 5;
+            //Console.WriteLine((a*Math.Pow(x,2))+b*x+c);
+
+            //4
+            //Console.Write("Имя?: ");
+            //string name = Console.ReadLine();
+            //Console.Write("возраст?: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("город?: ");
+            //string city = Console.ReadLine();
+            //Console.Write("любимое число?: ");
+            //int fav = int.Parse(Console.ReadLine());
+            //Console.Write("темпа?: ");
+            //int tem = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.WriteLine($"=== ПЕРСОНАЛЬНЫЙ ПРОФИЛЬ ===");
+            //Console.WriteLine();
+            //Console.WriteLine($"Привет {name}");
+            //Console.WriteLine($"Тебе {age} лет");
+            //Console.WriteLine($"В следующем году тебе будет {age+1} лет");
+            //Console.WriteLine();
+            //Console.WriteLine($"Ты живешь в городе {city}");
+            //Console.WriteLine($"Твое любимое число {fav}(в квадрате {Math.Pow(fav,2)})");
+            //Console.WriteLine();
+            //Console.WriteLine($"Температура за окном: ");
+            //Console.WriteLine($"-В цельсиях {tem}");
+            //Console.WriteLine($"-в фаренгейтах {tem * 1.8 + 32} ");
+            //Console.WriteLine();
+            //Console.WriteLine($"=== КОНЕЦ ПРОФИЛЯ ===");
+            //Console.WriteLine();
         }
     }
 }
